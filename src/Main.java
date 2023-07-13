31f62a5178cc75ee579b95b0ea3713b512019013
Realmarvin17 <zverevdm@mail.ru>
2023-07-13 12:41:07 +0300

    add homework Seminar_3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,123 @@
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
+        SingleLinkList<Contact> contactList = new SingleLinkList<>();
 
-        BinaryTree<Integer> tree = new BinaryTree<>();
+        contactList.addToEnd(new Contact(234, "Семенов Семен Семенович", "+79682365716"));
+        contactList.addToEnd(new Contact(345, "Петров Петр Петрович", "+79854186234"));
+        contactList.addToEnd(new Contact(456, "Сидоров Глеб Вениаминович", "+79765847596"));
+        contactList.addToEnd(new Contact(678, "Горбачев Станислав Степанович", "+79241569856"));
+        contactList.addToEnd(new Contact(789, "Павлова Лариса Анисимовна", "+79623584986"));
 
-        tree.add(6);
-        tree.add(3);
-        tree.add(2);
-        tree.add(4);
-        tree.add(5);
-        tree.add(1);
-        tree.add(9);
-        tree.add(7);
-        tree.add(11);
-        tree.add(8);
-        tree.add(12);
-        tree.add(15);
-        tree.add(10);
+        for (Object contact : contactList) {
+            System.out.println(contact);
+        }
+        contactList.reverse();
 
-        tree.print();
+        System.out.println("_-#-_-#-_-#-_-#-_-#-_-#-_-#-_-#-_-#-_-#-_-#-_");
 
+        for (Object contact : contactList) {
+            System.out.println(contact);
+        }
+    }
+
+    static class Contact {
+        int id;
+        String name;
+        String phone;
+
+        public Contact(int id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            return "Contact{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", phone='" + phone + '\'' +
+                    '}';
+        }
+    }
+
+    /**
+     * Класс списка
+     *
+     * @param <T>
+     */
+    public static class SingleLinkList<T> implements Iterable {
+        ListItem<T> head;
+        ListItem<T> tail;
+
+        @Override
+        public Iterator iterator() {
+            return new Iterator<T>() {
+                ListItem<T> current = head;
+
+                @Override
+                public boolean hasNext() {
+                    return current != null;
+                }
+
+                @Override
+                public T next() {
+                    T data = current.data;
+                    current = current.next;
+                    return data;
+                }
+            };
+        }
+
+        /**
+         * Класс отдельного элемента
+         *
+         * @param <T>
+         */
+        private static class ListItem<T> {
+            T data;
+            ListItem<T> next;
+        }
+
+        // Проверка на то, пустая ли голова
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        // Заполнение списка
+        public void addToEnd(T item) {
+
+            // Выделение памяти для списка
+            ListItem<T> newItem = new ListItem<>();
+            newItem.data = item;
+
+            // Если голова и хвост пустая, то присваеваем newItem
+            if (isEmpty()) {
+                head = newItem;
+                tail = newItem;
+
+                // Если не пустая, то передаём элементу адрес и ставим его в хвост
+            } else {
+                tail.next = newItem;
+                tail = newItem;
+            }
+        }
+
+        // Метод разворота списка
+        public void reverse() {
+            if (!isEmpty() && head.next != null) { // Если не пусто и голова не равна нулю
+                tail = head;
+                ListItem<T> current = head.next;
+                head.next = null;
+                while (current != null) {
+                    ListItem<T> next = current.next;
+                    current.next = head;
+                    head = current;
+                    current = next;
+                }
+            }
+        }
     }
 }
